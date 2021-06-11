7fe39e260bc48dec95b83e950856b39f455beb5c
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-06-11 17:45:55 +0300

    magic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
+'use strict';
+// adding html
+import templateFunction from './template/menu-template.hbs';
+import cards from './menu.json';
 import './sass/main.scss';
+
+const menuRef = document.querySelector('.js-menu');
+const cardsMarkup = createMenuCards(cards);
+
+menuRef.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createMenuCards(cards) {
+  return cards.map(templateFunction).join('');
+}
+
+// switch themes
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const bodyRef = document.querySelector('body');
+const checkboxRef = document.querySelector('#theme-switch-toggle');
+
+// set defaults
+checkboxRef.checked = false;
+applyDefaultTheme();
+
+// checkbox listener
+checkboxRef.addEventListener('change', checkboxChangeHandler);
+
+function checkboxChangeHandler() {
+  if (checkboxRef.checked) {
+    bodyRef.classList.add(Theme.DARK);
+    bodyRef.classList.remove(Theme.LIGHT);
+  } else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+  }
+  saveThemeToLocalStorage();
+}
+
+// local storage
+function saveThemeToLocalStorage() {
+  localStorage.setItem('theme', bodyRef.getAttribute('class'));
+}
+
+function applyDefaultTheme() {
+  const localStorageTheme = localStorage.getItem('theme');
+
+  // apply default theme
+  if (localStorageTheme == null) {
+    bodyRef.classList.add = Theme.LIGHT;
+  } else {
+    bodyRef.className = '';
+    bodyRef.classList.add(localStorageTheme);
+  }
+
+  // setup checkbox state
+  if (localStorageTheme === Theme.DARK) {
+    checkboxRef.checked = true;
+  }
+}
